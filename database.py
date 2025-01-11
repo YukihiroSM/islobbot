bea5b4b48e4ab1fb7f33b5bad56a73aafa16ed15
yuk0sm <yuk0.terraf@gmail.com>
2025-01-11 21:56:30 +0200

    Update pool usage of database

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from config import DATABASE_URL
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, pool_recycle=120)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
